1ade5143288bf374b55f16d4339b50b2822bd2a5
Nicolás Giovannoni <122325992+nicolas-giovannoni@users.noreply.github.com>
2026-01-01 22:28:54 -0300

    Fix API request to use fruit_chosen instead of search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
 
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
