75739130c2e0027e800500c0ee7b736e67d298a7
Nicolás Giovannoni <122325992+nicolas-giovannoni@users.noreply.github.com>
2026-01-01 21:25:39 -0300

    Update smoothie API response display method

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,5 +33,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-#sf_fd = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+# st.text(smoothiefroot_response.json())
+sf_fd = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
